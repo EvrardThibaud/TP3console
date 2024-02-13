8a5c527202c05fb7f3a81484cba0c8a6782f24b0
Thibaud Evrard <thibaud.evrard@etu.univ-smb.fr>
2024-02-13 15:19:54 +0100

    exemple modif + commande

--- TP3console/Program.cs
+++ TP3console/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace TP3console
+﻿using System;
+using TP3console.Models.EntityFramework;
+using System.Linq;
+
+namespace TP3console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var ctx = new FilmsDBContext())
+            {
+                Film titanic = ctx.Films.First(f => f.Nom.Contains("Titanic"));
+
+                titanic.Description = "Un bateau échoué. Date : " + DateTime.Now;
+
+                int nbchanges = ctx.SaveChanges();
+
+                Console.WriteLine("Nombre d'enregistrements modifiés ou ajoutés : " + nbchanges);
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
