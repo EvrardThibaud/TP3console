377b12e0f9b8e88d5aa8a782cbcb50e9337574fb
Thibaud Evrard <thibaud.evrard@etu.univ-smb.fr>
2024-02-15 08:30:13 +0100

    chargement explicite et hatif

--- TP3console/Program.cs
+++ TP3console/Program.cs
@@ -9,26 +9,46 @@ namespace TP3console
     {
         static void Main(string[] args)
         {
-            using (var ctx = new FilmsDBContext())
+            /*explicite*/
+            /*using (var ctx = new FilmsDBContext())
             {
-                /*ctx.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;*/
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                //Chargement des films dans categorieAction
+                ctx.Entry(categorieAction).Collection(c => c.Films).Load();
+                Console.WriteLine("Films : ");
+                foreach (var film in categorieAction.Films)
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }*/
 
+            /*Hatif*/
+            /*using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action et des films de cette catégorie
+                Categorie categorieAction = ctx.Categories
+                .Include(c => c.Films)
+                .First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                foreach (var film in categorieAction.Films)
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }*/
 
-                Film titanic = ctx.Films.AsNoTracking().First(f => f.Nom.Contains("Titanic"));
+            /*using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action, des films de cette catégorie et des avis
+                Categorie categorieAction = ctx.Categories
+                .Include(c => c.Films)
+                .ThenInclude(f => f.Avis)
+                .First(c => c.Nom == "Action");
+            }*/
 
-                /*Film titanic = (from f in ctx.Films
-                                where f.Nom == "Titanic"
-                                select f).First();*/
+            /*différé*/
 
-
-                titanic.Description = "Un bateau échoué. Date : " + DateTime.Now;
-
-                int nbchanges = ctx.SaveChanges();
-
-                Console.WriteLine("Nombre d'enregistrements modifiés ou ajoutés : " + nbchanges);
-
-            }
-            Console.ReadKey();
 
         }
     }
